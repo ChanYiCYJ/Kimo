cbc5482f21a978c4ac843cc0be8a7fe2f656e872
JasonChan <j498780@gmail.com>
2025-12-15 20:37:13 +0800

    使用copilot修改注册代码格式

--- kimo/views/account.py
+++ kimo/views/account.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request
 import pymysql
 from utils import db
-from werkzeug.security import generate_password_hash, check_password_hash
 ac=Blueprint('account',__name__)
 
 @ac.route('/login',methods=['GET','POST'])
@@ -21,13 +20,21 @@ def login():
 def register():
     if request.method == 'GET':
         return render_template('login.html')
-    username=request.form.get('username')
+    
+    username = request.form.get('username')
     email = request.form.get('email')
     password = request.form.get('password')
-    result =db.register_user('INSERT INTO userInfo(email,password,user_name) VALUES(%s,%s,%s)',[email,password,username])
-    login()
-
-    return render_template('login.html', error="请重新尝试")
+    
+    # 验证输入
+    if not username or not email or not password:
+        return render_template('login.html', error="请填写所有必需字段")
+    
+    result = db.register_user('INSERT INTO userInfo(email,password,user_name) VALUES(%s,%s,%s)', [email, password, username])
+    
+    if result:
+        return render_template('login.html', success="注册成功，请登录")
+    else:
+        return render_template('login.html', error="注册失败，请重试")
 
 @ac.route('/logout')
 def logout():
