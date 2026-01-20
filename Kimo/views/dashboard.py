640002f6b6626cf0c96b86c4f519328dd0bbfe87
ChanyiCYJ <j498780@gmail.com>
2026-01-20 14:50:21 +0800

    update dashboard鉴权

--- Kimo/views/dashboard.py
+++ Kimo/views/dashboard.py
@@ -5,18 +5,20 @@ ds=Blueprint('dashboard',__name__)
 
 @ds.route('/dashboard',methods=['GET'])
 def index():
-    if request.method=='GET':  
-        config =load_config('app','config')
-        page_type=request.args.get("type", type=str)
-        id=request.args.get("id", type=int)
-        print(page_type,id,'dfddd')
-        if not page_type:
-            page_type ='home'
-        if page_type =='manageArticle':
-            categories = Article.get_all_categories()
-            article_all = Article.get_all_articles()
-            return render_template("dashboard.html",post=article_all,editor='1',categories=categories,config=config,pageType=page_type)
-        return render_template("dashboard.html",editor='1',id=id,config=config,pageType=page_type)
+    check_user = session.get('user_role')
+    if check_user == 2:
+        if request.method=='GET':  
+            config =load_config('app','config')
+            page_type=request.args.get("type", type=str)
+            id=request.args.get("id", type=int)
+            print(page_type,id,'dfddd')
+            if not page_type:
+                page_type ='home'
+            if page_type =='manageArticle':
+                categories = Article.get_all_categories()
+                article_all = Article.get_all_articles()
+                return render_template("dashboard.html",post=article_all,editor='1',categories=categories,config=config,pageType=page_type)
+            return render_template("dashboard.html",editor='1',id=id,config=config,pageType=page_type)
             
 
 
